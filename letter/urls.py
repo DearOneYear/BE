05d8ca4d34877a253762b687021dc8708083cbf7
Suji Cha <suji0509@naver.com>
2023-01-12 21:08:22 +0900

    static files load

--- letter/urls.py
+++ letter/urls.py
@@ -19,7 +19,12 @@ from .views import LetterList, LetterDetail
 # photo
 from django.conf import settings
 from django.conf.urls.static import static
+
+from django.views.static import serve
+from django.urls import re_path
 urlpatterns = [
     path('letterbox/', LetterList.as_view()),
-    path('letter/', LetterDetail.as_view())
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('letter/', LetterDetail.as_view()),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root':settings.MEDIA_ROOT}), # for deploy
+    re_path(r'^static/(?:.*)$', serve, {'document_root': settings.STATIC_ROOT, }),  # for deploy
+] # + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
